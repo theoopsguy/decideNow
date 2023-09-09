c69a8af1937562dc28f115ce844cef4c542553f7
theoopsguy <theoopsguy01@gmail.com>
2023-09-09 13:23:48 +0530

    add polls module and use redis cache for polls

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -3,10 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { redisModule } from 'src/modules.config';
 import { PollsController } from './polls.contoller';
 import { PollsService } from './polls.service';
+import { PollsRepository } from './polls.repository';
 
 @Module({
   imports: [ConfigModule, redisModule],
   controllers: [PollsController],
-  providers: [PollsService],
+  providers: [PollsService, PollsRepository],
 })
 export class PollsModule {}
