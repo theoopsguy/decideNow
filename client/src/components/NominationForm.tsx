771f61529fa9be1623dd6d366363a168764ce1b0
theoopsguy <theoopsguy01@gmail.com>
2023-09-16 23:29:24 +0530

    fix failing build

--- client/src/components/NominationForm.tsx
+++ client/src/components/NominationForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { MdCancel } from 'react-icons/md';
-import { Nominations } from '../pollState';
+import { Nominations } from '../../../common/poll-types';
 import BottomSheet, { BottemSheetProps } from './ui/BottomSheet';
 
 type NominationFormProps = {
