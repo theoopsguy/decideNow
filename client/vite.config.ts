302d6f1f74049b6e4d9f7f4caccf8134e22a661a
theoopsguy <theoopsguy01@gmail.com>
2023-10-21 14:28:48 +0530

    make client port dynamic

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 8080,
-  },
-  css: {
-    modules: {
-      localsConvention: 'camelCaseOnly',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(env.VITE_CLIENT_PORT) || 8080,
     },
-  },
+    css: {
+      modules: {
+        localsConvention: 'camelCaseOnly',
+      },
+    },
+  };
 });
